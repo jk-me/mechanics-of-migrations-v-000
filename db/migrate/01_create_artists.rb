e7d4cc67e36d5be5b17edbe01f369dc49cca2238
Jenny K <jkam17@gmail.com>
2018-09-21 00:23:43 +0000

    automatically backed up by learn

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,11 @@
+class CreateArtists < ActiveRecord::Migration
+  def up #when migration is run 
+  end
+ 
+  def down #when migration is rolled back
+
+  end
+
+  def change #primary way tomigrate, active record automatically knows how to reverse
+  end
+end
